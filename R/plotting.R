a85b31fb84b523891e437de07aeb0fd0d83aab3c
PedroRaposo <pedro.raposo3@gmail.com>
2020-04-28 14:56:15 +0100

    function

---
+++ R/plotting.R
@@ -0,0 +1,18 @@
+
+vcf_histogram = function(vcf, ...){
+  require(VariantAnnotation)
+  require(dplyr)
+  require(ggplot2)
+
+  info = as_tibble(info(exVCF))
+
+  plot = info %>%
+    group_by(DP) %>%
+    summarise(n = n()) %>%
+    ggplot(aes(x = DP, y = n, fill = DP)) +
+    geom_bar(stat = "identity") +
+    theme(axis.text.x = element_blank())
+
+  return(plot)
+}
+
